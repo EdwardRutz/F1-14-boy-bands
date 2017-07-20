674536e4e1d19192782375d525fbefa996284781
Edward R <edwardrz@users.noreply.github.com>
2017-07-19 21:15:36 -0500

    added

--- js/main.js
+++ js/main.js
@@ -1,29 +1,20 @@
-var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
-var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
-// The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+let loopCount = 5;
+let currentBand = "";
+let currentVeggie = "";
 
-// Keep track of which band we're on in the loop
-var currentBand = "";
+// let bandElement = document.getElementById(???);
 
-// Keep track of which veggie we're on in the loop
-var currentVeggie = "";
+// let veggieElement = ???;
 
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
-
-// Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
-
-// Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+for (let loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
-  // Add the band names into the correct <div>
-  currentBand = ???;
 
-  
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+
+  currentBand = document.getElementById("boy-bands");
+
+  currentVeggie = document.getElementById("vegetables");
 
 }
